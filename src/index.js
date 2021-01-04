9b89944e6837b607d7b7a97673e8b012139e8349
chuanfeng.liu <chuanfeng1.liu@aihuishou.com>
2021-01-04 14:49:24 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -2,24 +2,26 @@ const core = require('@actions/core');
 const OSS = require('ali-oss');
 const ossdir = require('ali-oss-dir');
 
+const oss = new OSS({
+  region: core.getInput('region', { required: true }),
+  accessKeyId: core.getInput('accessKeyId', { required: true }),
+  accessKeySecret: core.getInput('accessKeySecret', { required: true }),
+  bucket: core.getInput('bucket', { required: true })
+})
+
+
 function uploadTool() {
   try {
-    const oss = new OSS({
-      region: core.getInput('region', { required: true }),
-      accessKeyId: core.getInput('accessKeyId', { required: true }),
-      accessKeySecret: core.getInput('accessKeySecret', { required: true }),
-      bucket: core.getInput('bucket', { required: true })
-    })
-
     const assetPath = core.getInput('assetPath')
-    core.info('assetPath', assetPath)
     const targetPath = core.getInput('targetPath')
-    core.info('targetPath', targetPath)
+
     ossdir(oss)
       .upload(assetPath)
       .to(targetPath)
       .then((results) => {
         core.info(results)
+      }).catch(e => {
+        uploadTool()
       })
   } catch (err) {
     core.setFailed(err.message)
